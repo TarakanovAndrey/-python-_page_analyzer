7ad2f8fa1e893a91f2f8bd29dfd6dea18ad48068
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-08-11 13:34:34 +0300

    Implemented the first version of the working version on the local server

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,85 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+import psycopg2
+from urllib.parse import urlparse
+from validators import url
+import os
+from datetime import datetime, date, time
+from page_analyzer.database_operations import PostgresqlOperations
+from itertools import chain
+from bs4 import BeautifulSoup
+import requests
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SJLDFJSLJDLJAJSLKDJFLSJF'
+
+db = PostgresqlOperations(user='andrey', password='password', database='database')
+
+
+def get_site_info(sites_url):
+
+    page = requests.get(sites_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    h1 = soup.find('h1').get_text() if soup.find('h1') else ''
+    title = soup.find('title').get_text() if soup.find('title') else ''
+    description = soup.find(attrs=({'name': 'description'})).get('content') if soup.find(attrs=({'name': 'description'})) else ''
+    status_code = page.status_code if page.status_code else ''
+    result = {'h1': h1, 'title': title, 'description': description, 'status_code': status_code}
+    return result
+
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Andrey!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def request_processing():
+    entered_request = request.form.to_dict()['url']
+    url_site = urlparse(entered_request).scheme + '://' + urlparse(entered_request).netloc
+    dates_create = datetime.now().date()
+
+    if url(url_site) is True:
+        db.insert_unique("sites_list", name=url_site, created_at=dates_create)
+
+    rows_id = db.select('sites_list', fields_name='*', condition=f"name = '{url_site}'")['id']
+    # еще раз посмотреть про вставку с возвратом последней вставленной строки
+    return redirect(url_for('show_site_info', site_id=rows_id, url_site=url_site))
+
+
+@app.route('/urls', methods=['GET'])
+def get_sites_list():
+    sites_list = db.select(table_name='sites_list', fields_name='*', fields_order='id', order='DESC')
+    return render_template('sites.html', sites_list=sites_list)
+
+
+@app.route('/urls/<site_id>', methods=['GET'])
+def show_site_info(site_id):
+    url_info = db.select('sites_list', fields_name='*', condition=f"id = {site_id}")
+
+    checks_list = db.select(table_name='checks_info', fields_name='*', condition=f"sites_list_id = {site_id}", fields_order='id', order='DESC')
+
+    return render_template('url_info.html', url_info=url_info, checks_list=checks_list)
+
+
+
+@app.route('/urls/<site_id>/checks', methods=['POST'])
+def check_url(site_id):
+    url_site = db.select('sites_list',  fields_name='*', condition=f"id = {site_id}")['name']
+
+    checks_result = get_site_info(url_site)
+
+    dates_create = datetime.now().date()
+    db.insert("checks_info",
+              sites_list_id=site_id,
+              code_response=checks_result['status_code'],
+              h1=checks_result['h1'],
+              title=checks_result['title'],
+              description=checks_result['description'],
+              created_at=dates_create)
+
+    db.update(table='sites_list', field_name=f"date_last_check = '{dates_create}', last_code_response = '{checks_result['status_code']}'", condition=f"name = '{url_site}'")
+
+    return redirect(url_for('show_site_info', site_id=site_id))
---
+++ page_analyzer/database_operations.py
@@ -0,0 +1,254 @@
+import psycopg2
+import sys
+from itertools import chain
+
+# class PostgresqlDBManagementSystem(object):
+#     """Питоновский класс для баловства с postgresql"""
+#
+#     __instance = None
+#     __host = None
+#     __user = None
+#     __password = None
+#     __database = None
+#     __cursor = None
+#     __connection = None
+#
+#     # def __new__(cls, *args, **kwargs):
+#     #     if not cls.__instance or not cls.__database:
+#     #         cls.__instance = super(PostgresqlDBManagementSystem, cls).__new__(cls, *args, **kwargs)
+#     #     return cls.__instance
+#
+#     def __init__(self, host='localhost', user='nester', password='', database=''):
+#         self.__host = host
+#         self.__user = user
+#         self.__password = password
+#         self.__database = database
+#
+#     def __open(self):
+#         self.__connection = psycopg2.connect("dbname='{0}' user='{1}'".format(self.__database, self.__user))
+#         self.__cursor = self.__connection.cursor()
+#         # try:
+#         #     self.__connection = psycopg2.connect("dbname='{0}' user='{1}'".format(self.__database, self.__user))
+#         #     self.__cursor = self.__connection.cursor()
+#
+#         # except:
+#         # except psycopg2.DatabaseError, e:
+#         #     if self.__connection:
+#         #         self.__connection.rollback()
+#         #     print( 'Error %s %e')
+#         #     sys.exit(1)
+#
+#     def __close(self):
+#         self.__connection.close()
+#
+#     def create_table(self, table_name, table_structure):
+#
+#         query = "CREATE TABLE " + table_name + table_structure
+#         self.__open()
+#         self.__cursor.execute("DROP TABLE IF EXISTS {0}".format(table_name))
+#         self.__cursor.execute(query)
+#         self.__connection.commit()
+#         self.__close()
+#
+
+
+#############################################################################
+def datas_to_dict(keys, values):
+    keys = tuple(x[0] for x in keys)
+    result = []
+    for item in values:
+        dict_ = {}
+        for key, value in zip(keys, item):
+            dict_[key] = value
+        result.append(dict_)
+    return result
+
+class PostgresqlOperations():
+
+    __instance = None
+    __host = None
+    __user = None
+    __password = None
+    __database = None
+    __cursor = None
+    __connection = None
+
+    def __init__(self, host='localhost',
+                 user='nester',
+                 password='',
+                 database=''):
+
+        self.__host = host
+        self.__user = user
+        self.__password = password
+        self.__database = database
+
+    def __open(self):
+        try:
+
+            self.__connection = psycopg2.connect(database=self.__database,
+                                                 user=self.__user,
+                                                 password=self.__password)
+            self.__cursor = self.__connection.cursor()
+
+
+        except psycopg2.DatabaseError:
+            print("Can't establish connection to database")
+            '''Прописать откат любых изменений в случае ошибки подключения. rollback'''
+
+    def __close(self):
+        self.__connection.close()
+
+
+    def create_table(self, table_name, datas_structure):
+        query = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(datas_structure)});"
+
+        self.__open()
+        self.__cursor.execute(query)
+
+        self.__connection.commit()
+        self.__close()
+
+    def drop_table(self, table_name):
+        query = f"DROP TABLE IF EXISTS {table_name}"
+        self.__open()
+        self.__cursor.execute(query)
+        self.__connection.commit()
+        self.__close()
+
+
+    def delete(self, table,
+               where: str = False):
+
+        """
+        Реализуется запрос формата
+        DELETE FROM <НАЗВАНИЕ ТАБЛИЦЫ> WHERE <УСЛОВИЕ ПО КОТОРОМУ УДАЛЯЕТСЯ СТРОКА ИЛИ СТРОКИ>
+        """
+        delete = f"DELETE FROM {table}"
+        where = f"WHERE {where}" if where else False
+        queries_body = (delete, where)
+        query = ' '.join(filter(lambda x: bool(x), queries_body)) + ';'
+        print(query)
+        self.__open()
+        self.__cursor.execute(query)
+
+        self.__connection.commit()
+        self.__close()
+
+
+
+    def insert(self, *args, **kwargs):
+        table_name = args[0]
+        fields_name = ', '.join(kwargs.keys())
+        values = ', '.join(f"'{x}'" for x in kwargs.values())
+
+        query = f"INSERT INTO {table_name} ({fields_name}) VALUES ({values})"
+        print(query)
+        self.__open()
+        self.__cursor.execute(query)
+        self.__connection.commit()
+        self.__close()
+
+    def insert_unique(self, *args, **kwargs):
+        table_name = args[0]
+        fields_name = ', '.join(kwargs.keys())
+        values = ', '.join(f"'{x}'" for x in kwargs.values())
+
+        query = f"INSERT INTO {table_name} ({fields_name}) VALUES ({values}) ON CONFLICT (name) DO NOTHING;"
+
+        self.__open()
+        self.__cursor.execute(query)
+
+        self.__connection.commit()
+        self.__close()
+
+
+    def get_heads_table(self, table_name):
+        """Вывод названия всех полей таблицы"""
+        table_name = table_name
+        query = (f"SELECT column_name "
+                 f"FROM information_schema.columns "
+                 f"WHERE table_schema = 'public' "
+                 f"AND table_name = '{table_name}' ORDER BY ordinal_position ASC")
+
+        self.__open()
+        self.__cursor.execute(query)
+        rows = self.__cursor.fetchall()
+        self.__connection.commit()
+        self.__close()
+        return rows
+
+
+    def select(self, table_name: str,
+               fields_name: str = None,
+               condition: str = None,
+               fields_order: str = None,
+               order: str = None,
+               fetch: str = 'all'):
+
+        '''
+         Реализован запрос вида: SELECT <fields_name> FROM <table_name> WHERE <condition> ORDER BY <fields_order> <order>
+        :param table_name: название таблицы
+        :param fields_name: название полей, данные по которым надо вывести. Перечисляются в кавычках через запятую
+        :param condition: условаия выборки данных в кавычках через запятую
+        :param fields_order: поле по которому упорядочивается
+        :param order: порядок вывода данных DESC - в обратном порядке. Если ничего не указывать, то по умолчанию выводится в прямом
+        :param fetch: если указать 'one', то возьмется верхняя строка из таблицы. По умолчанию выводятся все
+        :return: возвращается словарь, если одна строка или если требуется из всех строк вывести только строку с последней проверкой, и список словарей, если больше одной строки
+        '''
+
+        ''''''
+
+        query_body = f"SELECT {fields_name} FROM {table_name}"
+        condition = f"WHERE {condition}" if condition else False
+        order = f"ORDER BY {fields_order} {order}" if fields_order else False
+        query_items = (query_body, condition, order)
+        query = ' '.join(item for item in query_items if item) + ';'
+
+        self.__open()
+        self.__cursor.execute(query)
+
+        rows = self.__cursor.fetchall()
+
+        self.__connection.commit()
+        self.__close()
+        if len(rows) == 1 and fetch == 'all' or len(rows) > 1 and fetch == 'one':
+            return datas_to_dict(self.get_heads_table(table_name), rows)[0]
+
+        return datas_to_dict(self.get_heads_table(table_name), rows)
+
+    def update(self, table,
+               field_name,
+               condition: str = False):
+        """
+        Реализуется запрос вида
+        UPDATE <ИМЯ ТАБЛИЦЫ> SET <НА КАКОЕ ЗНАЧЕНИЕ В КАКОМ ПОЛЕ МЕНЯЕТСЯ> WHERE <УСЛОВИЕ КАКИЕ ПОЛЯ ОТБИРАЮТСЯ ДЛЯ ЗАМЕНЫ>
+        """
+        update = f"UPDATE {table}"
+        set_ = f"SET {field_name}"
+        condition = f"WHERE {condition}" if condition else False
+        queries_body = (update, set_, condition)
+        query = ' '.join(filter(lambda x: bool(x), queries_body)) + ';'
+        # print(query)
+        self.__open()
+        self.__cursor.execute(query)
+
+        self.__connection.commit()
+        self.__close()
+
+db = PostgresqlOperations(user='andrey', password='password', database='database')
+
+# db.create_table(table_name='sites_list', datas_structure=(('id bigint primary key generated always as identity'),
+#                                                      ('name varchar(255) unique'),
+#                                                      ('created_at date'))
+#                 )
+#
+# db.create_table(table_name='checks_info', datas_structure=(('id bigint primary key generated always as identity'),
+#                                                            ('sites_list_id bigint REFERENCES sites_list (id)'),
+#                                                            ('code_response varchar(255)'),
+#                                                            ('h1 text'),
+#                                                            ('title text'),
+#                                                            ('description text'),
+#                                                            ('created_at date')))
+
+# значение по умолчанию "" чтобы при добавлении ячеек не было none
