ecc95a54e951feeae1aa1b069851cdc62ccab3a3
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-08-18 00:56:13 +0300

    Add UNIQUE in create_table

--- database.sql
+++ database.sql
@@ -2,8 +2,10 @@ DROP TABLE IF EXISTS urls CASCADE;
 
 CREATE TABLE IF NOT EXISTS urls (
 id bigint primary key generated always as identity,
-name varchar(255) unique,
-created_at timestamp DEFAULT CURRENT_TIMESTAMP);
+name varchar(255) UNIQUE,
+created_at timestamp DEFAULT CURRENT_TIMESTAMP
+
+);
 
 
 DROP TABLE IF EXISTS url_checks CASCADE;
@@ -12,4 +14,5 @@ CREATE TABLE IF NOT EXISTS url_checks (
 id bigint primary key generated always as identity,
 url_id bigint REFERENCES urls (id),
 status_code varchar(255), h1 text, title text,
-description text, created_at timestamp DEFAULT CURRENT_TIMESTAMP);
+description text, created_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
