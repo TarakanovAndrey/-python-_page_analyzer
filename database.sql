12c299c2a5569750a60b0f24d94730493513eab4
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-08-17 21:28:29 +0300

    Update python version

--- database.sql
+++ database.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS urls;
 
 CREATE TABLE IF NOT EXISTS urls (
 id bigint primary key generated always as identity,
-name varchar(255) unique, created_at timestamp DEFAULT CURRENT_TIMESTAMP);
+name varchar(255) unique,
+created_at timestamp DEFAULT CURRENT_TIMESTAMP);
 
 
 DROP TABLE IF EXISTS url_checks;
@@ -12,5 +13,3 @@ id bigint primary key generated always as identity,
 url_id bigint REFERENCES urls (id),
 status_code varchar(255), h1 text, title text,
 description text, created_at timestamp DEFAULT CURRENT_TIMESTAMP);
-
-
