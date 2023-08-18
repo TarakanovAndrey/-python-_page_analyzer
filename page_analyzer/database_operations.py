a8921a13fde99e5a515355486981416622beda0e
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-08-18 20:41:57 +0300

    Corrected database class

--- page_analyzer/database_operations.py
+++ page_analyzer/database_operations.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import Error
 
 
 def datas_to_dict(items):
@@ -22,7 +23,6 @@ class PostgresqlOperations:
     def __init__(self, db_url):
         self.__db_url = db_url
 
-
     def insert(self, *args, **kwargs):
         table_name = args[0]
         fields_name = ', '.join(kwargs.keys())
@@ -31,9 +31,12 @@ class PostgresqlOperations:
         query = f"INSERT INTO {table_name} ({fields_name}) VALUES ({values}) RETURNING id;"
 
         with psycopg2.connect(self.__db_url) as connect:
-            cursor = connect.cursor()
-            cursor.execute(query)
-        connect.close()
+            try:
+                cursor = connect.cursor()
+                cursor.execute(query)
+
+            except (Exception, Error) as error:
+                print("Ошибка при работе с Postgresql", error)
 
     def insert_unique(self, *args, **kwargs):
         table_name = args[0]
@@ -44,9 +47,12 @@ class PostgresqlOperations:
                     VALUES ({values}) ON CONFLICT (name) DO NOTHING;'''
 
         with psycopg2.connect(self.__db_url) as connect:
-            cursor = connect.cursor()
-            cursor.execute(query)
-        connect.close()
+            try:
+                cursor = connect.cursor()
+                cursor.execute(query)
+
+            except (Exception, Error) as error:
+                print("Ошибка при работе с Postgresql", error)
 
     def get_heads_table(self, table_name):
         """Вывод названия всех полей таблицы"""
@@ -57,12 +63,14 @@ class PostgresqlOperations:
                  f"AND table_name = '{table_name}' ORDER BY ordinal_position ASC")
 
         with psycopg2.connect(self.__db_url) as connect:
-            cursor = connect.cursor()
-            cursor.execute(query)
-            rows = cursor.fetchall()
-        connect.close()
+            try:
+                cursor = connect.cursor()
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                return rows
 
-        return rows
+            except (Exception, Error) as error:
+                print("Ошибка при работе с Postgresql", error)
 
     def select(self, table_name: str,
                fields_name: (tuple, str) = None,
@@ -97,21 +105,23 @@ class PostgresqlOperations:
         query = ' '.join(item for item in query_items if item) + ';'
 
         with psycopg2.connect(self.__db_url) as connect:
-            cursor = connect.cursor()
-            cursor.execute(query)
-            if fetch == 'one':
-                rows = cursor.fetchone()
-            else:
-                rows = cursor.fetchall()
+            try:
+                cursor = connect.cursor()
+                cursor.execute(query)
+                if fetch == 'one':
+                    rows = cursor.fetchone()
+                else:
+                    rows = cursor.fetchall()
 
-        if fields_name == '*':
-            column_name = tuple([x[0] for x in self.get_heads_table(table_name='urls')])
-        else:
-            column_name = fields_name
-        out = datas_to_dict({'column_name': column_name, 'rows': rows})
-        connect.close()
+                if fields_name == '*':
+                    column_name = tuple([x[0] for x in self.get_heads_table(table_name='urls')])
+                else:
+                    column_name = fields_name
+                out = datas_to_dict({'column_name': column_name, 'rows': rows})
+                return out
 
-        return out
+            except (Exception, Error) as error:
+                print("Ошибка при работе с Postgresql", error)
 
     def select_special(self):
         query = '''SELECT urls.id, urls.name, url_checks.status_code, DATE(url_checks.created_at)
@@ -122,23 +132,27 @@ class PostgresqlOperations:
                     FROM url_checks WHERE urls.id = url_checks.url_id)
                     OR url_checks.created_at IS NULL ORDER BY urls.id DESC;'''
         with psycopg2.connect(self.__db_url) as connect:
-            cursor = connect.cursor()
-            cursor.execute(query)
-            rows = cursor.fetchall()
-        connect.close()
-
-        column_name = ('id', 'name', 'status_code_last_check', 'created_at_last_check')
-        out = datas_to_dict({'column_name': column_name, 'rows': rows})
-        return out
+            try:
+                cursor = connect.cursor()
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                column_name = ('id', 'name', 'status_code_last_check', 'created_at_last_check')
+                out = datas_to_dict({'column_name': column_name, 'rows': rows})
+                return out
+            except (Exception, Error) as error:
+                print("Ошибка при работе с Postgresql", error)
 
     def check_exists(self, table_name: str,
                      fields_name: (tuple, str) = None,
                      condition: str = None):
 
         query = f"SELECT EXISTS (SELECT {fields_name} FROM {table_name} WHERE {condition});"
+        cursor = self.__connect()
         with psycopg2.connect(self.__db_url) as connect:
-            cursor = connect.cursor()
-            cursor.execute(query)
-            rows = cursor.fetchall()
-        connect.close()
-        return {'answer': rows[0][0]}
+            try:
+                cursor = connect.cursor()
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                return {'answer': rows[0][0]}
+            except (Exception, Error) as error:
+                print("Ошибка при работе с Postgresql", error)
