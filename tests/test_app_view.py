1d8270229d1523a499ec8e41583204254b973a34
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-08-20 17:23:48 +0300

    Add tests

--- tests/test_app_view.py
+++ tests/test_app_view.py
@@ -1,7 +1,7 @@
 import pytest
 from page_analyzer.utility_function import datas_to_dict
 from page_analyzer.app import app
-from page_analyzer.database_operations import PostgresqlOperations
+# from page_analyzer.database_operations import PostgresqlOperations
 
 
 @pytest.fixture()
@@ -17,11 +17,11 @@ def get_app():
     yield app
 
 
-@pytest.fixture()
-def get_db():
-    db = PostgresqlOperations('postgres://page_analyzer_base_84ng_user:Sat3BKyF60ZjNqRLg0lyton7w4Voeu03@dpg-cjb32ngc'
-                              'fp5c73a6prk0-a.oregon-postgres.render.com/page_analyzer_base_84ng')
-    return db
+# @pytest.fixture()
+# def get_db():
+#     db = PostgresqlOperations('postgres://page_analyzer_base_84ng_user:Sat3BKyF60ZjNqRLg0lyton7w4Voeu03@dpg-cjb32ngc'
+#                               'fp5c73a6prk0-a.oregon-postgres.render.com/page_analyzer_base_84ng')
+#     return db
 
 
 @pytest.fixture()
@@ -39,14 +39,14 @@ def test_get_dict_from_select(get_dict_from_select):
     assert datas_to_dict(datas) == result
 
 
-def test_insert(get_db):
-    db = get_db
-    db.insert('test_urls', name='name1')
-    result = db.select(table_name='test_urls', fields_name='*')
-    assert result[0]['name'] == 'name1'
+# def test_insert(get_db):
+#     db = get_db
+#     db.insert('test_urls', name='name1')
+#     result = db.select(table_name='test_urls', fields_name='*')
+#     assert result[0]['name'] == 'name1'
 
 
-def test_correct_url(client, get_db):
+def test_correct_url(client):
     response = client.post('/urls', data={'url': 'https://flask.palletsprojects.com'})
     assert response.status_code == 302
 
