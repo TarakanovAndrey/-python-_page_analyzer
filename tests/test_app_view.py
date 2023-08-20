8b8638d02c7e0a017f2f0ade9e49198b8a3c3630
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-08-20 20:13:50 +0300

    Refactoring of code

--- tests/test_app_view.py
+++ tests/test_app_view.py
@@ -1,18 +1,11 @@
 import pytest
+import os
 from page_analyzer.database_operations import PostgresqlOperations
 from dotenv import load_dotenv
-import os
-
 
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-
-@pytest.fixture()
-def get_dict_from_select():
-    datas = {'column_name': ('id', 'name'), 'rows': (('1', 'name1'), ('2', 'name2'))}
-    result = ({'id': '1', 'name': 'name1'}, {'id': '2', 'name': 'name2'})
-    return datas, result
 
 
 @pytest.fixture()
@@ -23,5 +16,4 @@ def get_db():
 
 def test_insert(get_db):
     db = get_db
-    result = db.select(table_name='urls', fields_name='*')
-    assert len(result) > 0
+    assert isinstance(db.select(table_name='urls', fields_name='*'), list)
